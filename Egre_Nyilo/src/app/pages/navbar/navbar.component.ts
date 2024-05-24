c25a9306c604c9ff0124d049bd2f9d8f536e5e3b
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2024-05-24 03:41:39 +0200

    Egre_Nyilo_commit

--- Egre_Nyilo/src/app/pages/navbar/navbar.component.ts
+++ Egre_Nyilo/src/app/pages/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  menuOpen: boolean = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  isMobile() {
+    return window.innerWidth < 768; // Változtathatod az értéket aszerint, hogy melyik viewport-ot tekinted mobilnak
+  }
 }
