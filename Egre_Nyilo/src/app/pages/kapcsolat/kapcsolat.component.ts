50839e0f8202579ed514545ba2baa6b236f101ba
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2024-07-10 16:26:32 +0200

    emailsender

--- Egre_Nyilo/src/app/pages/kapcsolat/kapcsolat.component.ts
+++ Egre_Nyilo/src/app/pages/kapcsolat/kapcsolat.component.ts
@@ -9,5 +9,35 @@ import { BackgroundComponent } from '../background/background.component';
   styleUrl: './kapcsolat.component.scss'
 })
 export class KapcsolatComponent {
+  sender() {
+    const data = {
+      name: 'bármi',
+      email: 'test@gmail.com',
+      phone: '1341516145',
+      text: 'szövegszöveg'
+    }
+    fetch("https://brass-loud-whip.glitch.me/email", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        alert("Sikeres e-mail küldés!");
 
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("A funkció jelenleg nem elérhető!");
+      });
+  }
+
+  data:any = {
+  }
+
+  changer(data:any, name: string){
+    this.data[name] = data.target.value
+  }
 }
