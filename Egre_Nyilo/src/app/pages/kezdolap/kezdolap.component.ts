2a7aadf25b2548a13d5a566dd721ea4132270a09
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2024-09-06 01:39:16 +0200

    finish

--- Egre_Nyilo/src/app/pages/kezdolap/kezdolap.component.ts
+++ Egre_Nyilo/src/app/pages/kezdolap/kezdolap.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BackgroundComponent } from '../background/background.component';
 import { RouterLink } from '@angular/router';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-kezdolap',
@@ -9,6 +10,12 @@ import { RouterLink } from '@angular/router';
   templateUrl: './kezdolap.component.html',
   styleUrl: './kezdolap.component.scss'
 })
-export class KezdolapComponent {
+export class KezdolapComponent implements OnInit {
+  constructor(private meta: Meta, private titleService: Title) { }
 
+  ngOnInit(): void {
+    this.titleService.setTitle('Égre Nyíló Mentálhigiénés Műhely');
+    this.meta.addTag({ name: 'description', content: 'Ez az oldal leírása, amely tartalmazza a fő kulcsszót.' });
+    this.meta.addTag({ name: 'keywords', content: 'Keresztény pszichológus, Pszichológus, Tapolca, Szülés, szülési trauma, szülésélmény, Túlsúly, fogyás, Függőség, Pornófüggőség, Egyedülálló, szülő, Önismeret, Depresszió, Szorongás, Válás, Halál, gyász, veszteség'});
+  }
 }
